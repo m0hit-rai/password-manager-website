5fd25dcea142d5d85b460fe1f506e8beacbc88eb
Mohit Rai <1905250@kiit.ac.in>
2022-05-19 21:01:50 +0530

    add new password option added in profile

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
 const passport = require('passport');
@@ -9,14 +8,14 @@ const session = require('express-session');
 const bodyParser = require('body-parser');
 const credentials = require('./mongo-password');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
 const app = express();
 
 const uri = `mongodb+srv://${credentials.username}:${credentials.pass}@pass-manage.6z6kk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-let collection;
+let user_credentials_collection, user_data_collection;
 client.connect().then(() => {
-    collection = client.db("password-manage").collection("user-credentials");
+    user_credentials_collection = client.db("password-manage").collection("user-credentials");
+    user_data_collection = client.db("password-manage").collection("user-stored-pass");
 }).catch((err) => console.log(err));
 
 // middleware
@@ -39,7 +38,7 @@ app.use(cookieParser("v3ryL0ngRand0mS3cretK3y"));
 
 // routes
 app.post('/login', async (req, res) => {
-    collection.findOne({ username: req.body.username }, async (err, doc) => {
+    user_credentials_collection.findOne({ username: req.body.username }, async (err, doc) => {
         if (err) {
             res.status(500).send("Some error occured!");
             throw err;
@@ -65,8 +64,7 @@ app.post('/login', async (req, res) => {
 
 
 app.post('/register', async (req, res) => {
-
-    collection.findOne({ username: req.body.username }, async (err, doc) => {
+    user_credentials_collection.findOne({ username: req.body.username }, async (err, doc) => {
         if (err) {
             res.status(500).send("Some error occured!");
             throw err;
@@ -81,14 +79,69 @@ app.post('/register', async (req, res) => {
                 password: protectedHashedPass,
             };
 
-            await collection.insertOne(newUser);
+            await user_credentials_collection.insertOne(newUser);
             res.send("User Created successfully");
         }
     })
 });
-app.get('/user', ((req, res) => {
-    console.log(req.body);
-}));
+
+app.post('/addPass', (req, res) => {
+    // request will have
+    // current username(the one inserting the password),
+    // the site's name/address, the sites's username, the site's password, a short note about the site.
+    // console.log(req);
+    const newDoc = {
+        username: req.body.username,
+        site: req.body.site,
+        siteUsername: req.body.siteUsername,
+        sitePassword: req.body.sitePassword,
+        note: req.body.note,
+    }
+    // console.log(newDoc);
+    user_data_collection
+        .insertOne(newDoc)
+        .then(() => {
+            res.send("Password Added");
+        })
+        .catch((err) => {
+            res.status(500).send("Some error occured!");
+            throw err;
+        })
+    // res.send("testing phase")
+});
+
+app.get('/user/:username', async (req, res) => {
+    // request body will contain a username, display all its password
+    // console.log(req.params);
+    // res.status(404).send("Did it motherfudger!")
+    const query = { username: req.params.username };
+    const options = {
+        sort: { site: 1 },
+        projection: {
+            _id: 0,
+            username: 1,
+            site: 1,
+            siteUsername: 1,
+            sitePassword: 1,
+            note: 1,
+        }
+    }
+
+    try {
+        const cursor = user_data_collection.find(query, options);
+        let data = [];
+
+        await cursor.forEach(element => {
+            data.push(element);
+            // console.log(element);
+        });
+
+        res.send(data);
+    }
+    catch (error) {
+        res.status(500).send("Error occurred");
+    }
+});
 
 
 app.listen(33845, () => {
--- frontend/src/components/ProfileView.js
+++ frontend/src/components/ProfileView.js
@@ -1,8 +1,117 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function ProfileView(props) {
-    const { userName } = props
-    return <h1>Hello {userName} !</h1>;
+    const { userName } = props;
+    const [site, setSite] = useState("");
+    const [siteUsername, setSiteUsername] = useState('');
+    const [sitePassword, setSitePassword] = useState("");
+    const [data, setData] = useState([]);
+    const [note, setNote] = useState("");
+
+    useEffect(() => {
+        getAllData();
+    })
+
+    const addNewPass = (event) => {
+        event.preventDefault();
+        const newData = {
+            username: userName,
+            site: site,
+            siteUsername: siteUsername,
+            sitePassword: sitePassword,
+            note: note,
+        };
+        axios({
+            method: "post",
+            data: newData,
+            withCredentials: true,
+            url: "http://localhost:33845/addPass"
+        }).then((res) => {
+            // console.log(res);
+            setNote("");
+            setSite("");
+            setSitePassword("");
+            setSiteUsername("");
+            getAllData();
+        }).catch((err) => {
+            console.log(err);
+            alert("Some error Occurred");
+        })
+    }
+
+    const getAllData = (event) => {
+        // event.preventDefault();
+
+        axios({
+            method: "get",
+            url: `http://localhost:33845/user/${userName}`
+
+        }).then(res => {
+            // console.log(res);
+            setData(res.data);
+
+        }).catch(err => {
+            console.log(err);
+        })
+
+    };
+    return <>
+        <h1>Hello user {userName} !</h1>
+        <form>
+            <input
+                type="text"
+                placeholder='site'
+                value={site}
+                onChange={(e) => setSite(e.target.value)}
+            ></input>
+            <input
+                type="text"
+                placeholder='site-username'
+                value={siteUsername}
+                onChange={(e) => setSiteUsername(e.target.value)}
+            ></input>
+            <input
+                type="password"
+                placeholder='site-password'
+                value={sitePassword}
+                onChange={(e) => setSitePassword(e.target.value)}
+            ></input>
+            <input
+                type="text"
+                placeholder='note'
+                value={note}
+                onChange={(e) => setNote(e.target.value)}
+            ></input>
+            <button
+                disabled={site.length === 0 || siteUsername.length === 0 || sitePassword.length === 0}
+                onClick={addNewPass}
+            >Add New Pass</button>
+            {/* <button
+                onClick={getAllData}
+            >getAllData</button> */}
+        </form>
+        {data.map((element) => {
+            return <div>
+                <div>{element.site}</div>
+                <input
+                    type="text"
+                    value={element.siteUsername}
+                >
+                </input>
+                <input
+                    type="text"
+                    value={element.sitePassword}
+                >
+                </input>
+                <input
+                    type="text"
+                    value={element.note}
+                >
+                </input>
+            </div>
+        })}
+    </>;
 }
 
 export default ProfileView;
